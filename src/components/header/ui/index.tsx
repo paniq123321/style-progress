f7747ff99efbee0a67ca5874699d1ba7c2d4b6d7
paniq123321 <ecepaniq@gmail.com>
2025-09-22 10:13:06 -0300

    upd

--- src/components/header/ui/index.tsx
+++ src/components/header/ui/index.tsx
@@ -4,6 +4,7 @@ export const HeaderComponent = () => {
   const progress = useProgressbarStore(state => state.progress)
 
   const deg = (progress / 20) * 360
+  const transform = (100 - progress) / 10
 
   return (
     <header className="bg-gray-200 flex justify-between items-center px-6 py-4 overflow-hidden"
@@ -14,7 +15,7 @@ export const HeaderComponent = () => {
       <div
         className=""
            style={{
-             transform: `translateX(-${(100 - progress) / 10 }rem)`,
+             transform: `translateX(-${transform}rem)`,
            }}
       >
         <span>
@@ -25,51 +26,76 @@ export const HeaderComponent = () => {
       <ul className="flex gap-6 mx-2">
         <li
         style={{
-          rotate: `${deg <= 360? deg : 360}deg`,
-
+          transform: `translateY(-${(20 - progress) / 10}rem)`,
         }}
-        ><a>
+        >
+          <div style={{
+            rotate: `${deg <= 360? deg : 360}deg`,
+          }}>
+          <a>
           link 1
-        </a></li>
-        <li
-          style={{
-            rotate: `${deg <= 720? deg : 720}deg`,
-
-          }}
-        ><a>
-          link 2
-        </a></li>
-        <li
-          style={{
-            rotate: `${deg <= 1080? deg : 1080}deg`,
-
-          }}
-        ><a>
-          link 3
         </a>
+          </div>
         </li>
         <li
           style={{
-            rotate: `${deg <= 1440? deg : 1440}deg`,
-
+            transform: `translateY(-${(40 - progress) / 10}rem)`,
           }}
-        ><a>
-          link 4
-        </a></li>
+        > <div
+          style={{
+            rotate: `${deg <= 720? deg : 720}deg`,
+          }}
+        >
+          <a>
+            link 2
+          </a>
+        </div>
+        </li>
         <li
           style={{
-            rotate: `${deg <= 1800? deg : 1800}deg`,
-
+            transform: `translateY(-${(60 - progress) / 10}rem)`,
           }}
-        ><a>
-          link 5
-        </a></li>
+        > <div
+          style={{
+            rotate: `${deg <= 1080? deg : 1080}deg`,
+          }}
+        >
+          <a>
+            link 3
+          </a>
+        </div>
+        </li>
+        <li
+          style={{
+            transform: `translateY(-${(80 - progress) / 10}rem)`,
+          }}
+        > <div
+          style={{
+            rotate: `${deg <= 1440? deg : 1440}deg`,
+          }}
+        >
+          <a>
+            link 4
+          </a>
+        </div></li>
+        <li
+          style={{
+            transform: `translateY(-${(100 - progress) / 10}rem)`,
+          }}
+        > <div
+          style={{
+            rotate: `${deg <= 1800? deg : 1800}deg`,
+          }}
+        >
+          <a>
+            link 5
+          </a>
+        </div></li>
       </ul>
       </nav>
-
       <div
            style={{
-             transform: `translateX(${(100 - progress) / 10 }rem)`,
+             transform: `translateX(${transform}rem)`,
            }}
       >
         <button>
