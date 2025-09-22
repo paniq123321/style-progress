59268d2634609aa78985d445f2f8555b8a32d0cb
paniq123321 <ecepaniq@gmail.com>
2025-09-22 08:47:24 -0300

    upd

--- src/components/progress-bar/ui/index.tsx
+++ src/components/progress-bar/ui/index.tsx
@@ -1,8 +1,40 @@
 import { useEffect, useState } from "react";
 
 export const ProgressBarComponent = () => {
-  const [progress, setProgress] = useState(40);
+  const [progress, setProgress] = useState(0);
   const [isDragging, setIsDragging] = useState(false);
+
+
+
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      if (!isDragging) return;
+
+      const progressBar = document.getElementById("progress-bar");
+      if (!progressBar) return;
+
+      const rect = progressBar.getBoundingClientRect();
+      const offsetX = e.clientX - rect.left;
+      let newProgress = (offsetX / rect.width) * 100;
+
+      if (newProgress < 0) newProgress = 0;
+      if (newProgress > 100) newProgress = 100;
+
+      setProgress(newProgress);
+    };
+
+    const handleMouseUp = () => setIsDragging(false);
+
+    if (isDragging) {
+      window.addEventListener("mousemove", handleMouseMove);
+      window.addEventListener("mouseup", handleMouseUp);
+    }
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+      window.removeEventListener("mouseup", handleMouseUp);
+    };
+  }, [isDragging]);
 
 
   return(
@@ -12,15 +44,14 @@ export const ProgressBarComponent = () => {
       className=" relative h-3  w-[90%] max-w-[25rem] bg-gray-200 rounded-full"
     >
     <li
-      className="relative h-full  bg-black/20 rounded-full"
+      className="relative h-full  bg-black/20 rounded-full "
       style={{ width: `${progress}%` }}
     >
       <div
-        className="w-4 h-4 rounded-full absolute right-0 -top-0.5 bottom-0 bg-(--s-bg) cursor-pointer"
+        className="w-4 h-4 rounded-full absolute -right-2 -top-0.5 bottom-0 bg-(--s-bg) cursor-pointer"
         onMouseDown={() => setIsDragging(true)}
       />
     </li>
   </ul>
     </section>)
-
 };
