9a5197323f1be79f3cdac89abaf49e8e14fe402c
paniq123321 <ecepaniq@gmail.com>
2026-01-02 08:46:03 -0300

    upd

--- src/components/card/ui/index.tsx
+++ src/components/card/ui/index.tsx
@@ -1,4 +1,11 @@
+
+
+
+
 export const CardComponent = ({ content, className, ...m }: { content: string; className?: string }) => {
+
+
+
   return (
     <div
       className={`min-w-[6rem] min-h-[5rem] flex flex-1 justify-center items-center bg-gray-300/80 rounded ${className}`}
--- src/components/main/ui/index.tsx
+++ src/components/main/ui/index.tsx
@@ -1,11 +1,16 @@
+import { useProgressbarStore } from "../../../stores";
 import { CardComponent } from "../../card";
 
 export const MainComponent = () => {
-
+ const progress = useProgressbarStore(state => state.progress);
 
   return (
     <main className="py-4">
-      <section className="flex gap-5 bg-gray-100 p-4 mb-5">
+      <section className="flex gap-5 bg-gray-100 p-4 mb-5"
+               style={{
+                 transform: `translateX(${100 - progress}%)`,
+               }}
+      >
         {Array.from(Array(3).keys()).map((_, i) => (
           <CardComponent
             content={`${i + 1}`}
@@ -14,7 +19,11 @@ export const MainComponent = () => {
         ))}
       </section>
 
-      <section className="flex gap-5 bg-gray-100 p-4">
+      <section className="flex gap-5 bg-gray-100 p-4"
+               style={{
+                 transform: `translateX(${-100 + progress}%)`,
+               }}
+      >
         {Array.from(Array(3).keys()).map((_, i) => (
           <CardComponent
             content={`${i + 1}`}
