aea97351b5139561d4104df5a9e33eee10cb1f17
paniq123321 <ecepaniq@gmail.com>
2026-01-02 09:59:08 -0300

    upd

--- src/components/footer/ui/index.tsx
+++ src/components/footer/ui/index.tsx
@@ -4,7 +4,7 @@ export const FooterComponent = () => {
   const progress = useProgressbarStore(state => state.progress);
   return(
     <footer
-      className="bg-black p-6 text-[white] "
+      className="bg-black p-6 text-[white] sticky-bottom flex items-center justify-center min-h-[12rem]"
       style={{
         opacity: `${progress}%`
       }}
